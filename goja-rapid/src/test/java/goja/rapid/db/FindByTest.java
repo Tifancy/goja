ad8b618c7418a31fc66ca97c34e46904933f1be6
sogYF <poplar1123@gmail.com>
2015-02-04 15:08:55 +0800

    fixed test case

--- goja-rapid/src/test/java/goja/rapid/db/FindByTest.java
+++ goja-rapid/src/test/java/goja/rapid/db/FindByTest.java
@@ -12,19 +12,19 @@ public class FindByTest {
     public void testMain() throws Exception {
 
         MatcherAssert.assertThat("title = ?", Matchers.equalTo(findBy("byTitle")));
-        MatcherAssert.assertThat("LOWER(title) like ?", Matchers.equalTo(findBy("byTitleLike")));
-        MatcherAssert.assertThat("author is null", Matchers.equalTo(findBy("byAuthorIsNull")));
-        MatcherAssert.assertThat("LOWER(title) like ? AND author = ?", Matchers.equalTo(findBy("byTitleLikeAndAuthor")));
+        MatcherAssert.assertThat("LOWER(title) LIKE ?", Matchers.equalTo(findBy("byTitleLike")));
+        MatcherAssert.assertThat("author IS NULL", Matchers.equalTo(findBy("byAuthorIsNull")));
+        MatcherAssert.assertThat("LOWER(title) LIKE ? AND author = ?", Matchers.equalTo(findBy("byTitleLikeAndAuthor")));
         MatcherAssert.assertThat("name = ? ORDER BY name", Matchers.equalTo(findBy("ByNameOrderByName")));
         MatcherAssert.assertThat("name < ?", Matchers.equalTo(findBy("ByNameLessThan")));
         MatcherAssert.assertThat("name <= ?", Matchers.equalTo(findBy("ByNameLessThanEquals")));
         MatcherAssert.assertThat("name > ?", Matchers.equalTo(findBy("ByNameGreaterThan")));
         MatcherAssert.assertThat("name >= ?", Matchers.equalTo(findBy("ByNameGreaterThanEquals")));
-        MatcherAssert.assertThat("LOWER(name) like LOWER(?)", Matchers.equalTo(findBy("ByNameIlike")));
-        MatcherAssert.assertThat("name like ?", Matchers.equalTo(findBy("ByNameElike")));
+        MatcherAssert.assertThat("LOWER(name) LIKE LOWER(?)", Matchers.equalTo(findBy("ByNameIlike")));
+        MatcherAssert.assertThat("name LIKE ?", Matchers.equalTo(findBy("ByNameElike")));
         MatcherAssert.assertThat("name <> ?", Matchers.equalTo(findBy("ByNameNotEqual")));
-        MatcherAssert.assertThat("name ? AND name ?", Matchers.equalTo(findBy("ByNameBetween")));
-        MatcherAssert.assertThat("name is not null", Matchers.equalTo(findBy("ByNameIsNotNull")));
+        MatcherAssert.assertThat("name BETWEEN ? AND ?", Matchers.equalTo(findBy("ByNameBetween")));
+        MatcherAssert.assertThat("name IS NOT NULL", Matchers.equalTo(findBy("ByNameIsNotNull")));
 
     }
 
